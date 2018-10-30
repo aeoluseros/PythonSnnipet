bf6a877402dfbc4964ca261974b134e046bf43c8
aeoluseros <yklouk@gmail.com>
2018-10-29 23:53:58 -0400

    Create MarkovDecisionProcess.py

---
+++ DataScience/MarkovDecisionProcess.py
@@ -0,0 +1,3 @@
+# PAC-MAN https://inst.eecs.berkeley.edu/~cs188/sp12/projects/reinforcement/reinforcement.html
+# Cat-Mouse https://github.com/studywolf/blog/tree/master/RL/Cat%20vs%20Mouse%20exploration
+# Markov Decision Process (MDP) Toolbox https://pymdptoolbox.readthedocs.io/en/latest/api/mdp.html
